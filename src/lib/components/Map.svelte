a1b2e95a9f6ee5ca3abaa67716025dec1599edb2
Richard <55552821+Em1tt@users.noreply.github.com>
2022-10-01 05:10:05 +0200

    Update Map.svelte

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -19,10 +19,12 @@
 		}).addTo(Map);
 
 		const res = await axios.get("/api/parking");
-		res.data.filter((i: any) => !i.occupied)forEach((spot: any) => {
-			console.log(spot)
-			circleMarker([spot.latitude,spot.longitude]).addTo(Map);
-		})
+		res.data.forEach((spot: any) => {
+			circleMarker([spot.latitude,spot.longitude], {
+				color: spot.occupied ? "#c85050" : "#50C878"
+			}).bindPopup(`<h2><b>${spot.externalId ? spot.externalId : "Bez-ID"}: ${spot.occupied ? "OBSADENÉ" : "VOĽNÉ"}</b></h2><br><hr><br><p><b>Naposledy zmenené:</b> ${new Date(spot.lastStatusChange).toLocaleString()}</p><br><sub>${spot.provider} | N ${spot.latitude.toFixed(6)} | W ${spot.longitude.toFixed(6)}</sub>
+			<br><br><a target="_blank" href="https://www.google.com/maps/search/?api=1&query=${spot.latitude}%2C${spot.longitude}" style="border-radius: 6px; background-color: #00dd55; width: 260px !important; padding:5px;color:white;">Naviguj ma</a>`).addTo(Map);
+		});
 	});
 </script>
 
