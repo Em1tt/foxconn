bba8ef5e3848660fabe56cfb2114f1da8f56e95c
Richard <55552821+Em1tt@users.noreply.github.com>
2022-10-01 09:45:51 +0200

    .

--- src/lib/components/Filter.svelte
+++ src/lib/components/Filter.svelte
@@ -1,21 +1,49 @@
 <script lang="ts">
-    let toggle: boolean = false;
+	import { element } from "svelte/internal";
+
+
+    let toggle: boolean;
     //const button = document.querySelector("button");
-    function toggleFilter(toggle: boolean, event: any){
+    function toggleFilter(event: any){
         const button = document.querySelector("#filterWrapper");
         if(toggle){
+            toggle = false;
             //Hide
+            button?.classList?.remove("w-80");
+            button?.classList?.remove("h-80");
+            button?.classList?.add("w-0");button?.classList?.add("h-0");
         }else{
+            toggle = true;
             //Show
-            button?.classList?.remove("w-12");
-            button?.classList?.remove("h-12");
+            button?.classList?.remove("w-0");
+            button?.classList?.remove("h-0");
             button?.classList?.add("w-80");button?.classList?.add("h-80");
         }
-        toggle = !toggle;
     };
+    function toggleMarkers(query: any, checkbox: string){ //
+        if(!(document.getElementById(checkbox) as HTMLInputElement)?.checked){
+            [...document.querySelectorAll(query)].forEach((e) => e.classList.add("hidden"));
+        }else{
+            [...document.querySelectorAll(query)].forEach((e) => e.classList.remove("hidden"));
+        }
+    }
 </script>
 
-<button on:click={(event) => {toggleFilter(toggle, event)}} class="fixed left-24 top-4 fill-white z-40 w-12 h-12 bg-blue p-3 rounded-md">
+<button on:click={(event) => {toggleFilter(event)}} class="fixed left-24 top-4 fill-white z-40 w-12 h-12 bg-blue p-3 rounded-md">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Free 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --><path d="M3.9 54.9C10.5 40.9 24.5 32 40 32H472c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9V448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6V320.9L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z"/></svg>
 </button>
-<div id="filterWrapper" class="duration-300 top-4 left-24 rounded-md w-0 h-0"></div>
+<div id="filterWrapper" class="fixed duration-300 top-4 left-24 rounded-md w-0 h-0 bg-blue z-30 overflow-hidden">
+    <h2 class="ml-12 text-white text-lg font-bold pt-3">Filtre</h2><br>
+    <form class="p-2">
+        <p class="font-bold text-white mt-2">Obsadenosť</p>
+        <div class="flex flex-row items-center mt-2">
+            <input on:change={(e) => {toggleMarkers(".vacant", "vacant")}} type="checkbox" checked id="vacant" value="vacant" name="vacant" class="hidden peer"><label class="checkbox block w-6 h-6 border-4 border-blue rounded-md bg-blue brightness-150 peer-checked:bg-red" for="vacant"></label><label for="vacant" class="text-white ml-2">Voľné</label><br>
+        </div>
+        <div class="flex flex-row items-center mt-2">
+        <input on:change={() => {toggleMarkers(".occupied", "occupied")}} type="checkbox" id="occupied" value="occupied" name="occupied" class="hidden peer"><label class="checkbox block w-6 h-6 border-4 border-blue rounded-md bg-blue brightness-150 peer-checked:bg-red" for="occupied"></label><label for="occupied" class="text-white ml-2">Obsadené</label><br>
+        </div>
+        <div class="flex flex-row items-center mt-2">
+            <input on:change={(e) => {toggleMarkers(".idk", "idk")}} type="checkbox" checked id="idk" value="idk" name="idk" class="hidden peer"><label class="checkbox block w-6 h-6 border-4 border-blue rounded-md bg-blue brightness-150 peer-checked:bg-red" for="idk"></label><label for="idk" class="text-white ml-2">Neaktivné</label><br>
+        </div>
+    </form>
+</div>
--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -2,8 +2,10 @@
 	import { onMount } from 'svelte';
 	import axios from 'axios';
 	let mapElement: HTMLDivElement;
+	let mapp: any;
+	let group: any;
 	onMount(async () => {
-		const { map, tileLayer, circleMarker} = await import('leaflet');
+		const { map, tileLayer, circleMarker, marker} = await import('leaflet');
 		const Map = map(mapElement, {
 			maxBounds: [
 				[50.081702, 15.619725],
@@ -12,22 +14,46 @@
             minZoom: 14,
             maxBoundsViscosity: 100
 		}).setView([ 50.03861, 15.77916], 15);
+		mapp = Map;
 		tileLayer('https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png', {
 			maxZoom: 18,
 			attribution:
 				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 		}).addTo(Map);
 
+		
 		const res = await axios.get("/api/parking");
 		res.data.forEach((spot: any) => {
 			circleMarker([spot.latitude,spot.longitude], {
-				color: spot.occupied ? "#c85050" : "#50C878",
-				className: spot.occupied ? "occupied" : "vacant"
-			}).bindPopup(`<h2><b>${spot.externalId ? spot.externalId : "Bez-ID"}: ${spot.occupied ? "OBSADENÉ" : "VOĽNÉ"}</b></h2><br><hr><br><p><b>Naposledy zmenené:</b> ${new Date(spot.lastStatusChange).toLocaleString()}</p><br><sub>${spot.provider} | N ${spot.latitude.toFixed(6)} | W ${spot.longitude.toFixed(6)}</sub>
-			<br><br><a target="_blank" href="https://www.google.com/maps/search/?api=1&query=${spot.latitude}%2C${spot.longitude}" style="border-radius: 6px; background-color: #00dd55; width: 260px !important; padding:5px;color:white;">Naviguj ma</a>`).addTo(Map);
+				color: spot.occupied ? "#c85050" : Date.now() - spot.lastStatusChange > 15724800000 ? "#8c8c8c" : "#64c850",
+				className: spot.occupied ? "occupied hidden" : Date.now() - spot.lastStatusChange > 15724800000 ? "idk" : "vacant"
+			}).bindPopup(`<h2><b>${spot.externalId ? spot.externalId : "Bez-ID"}: ${spot.occupied ? "OBSADENÉ" : Date.now() - spot.lastStatusChange > 15724800000 ? "DLHO NEZMENENÝ STAV - NEZNÁMA SITUÁCIA" : "VOĽNÉ"}</b></h2><br><hr><br><p><b>Naposledy zmenené:</b> ${new Date(spot.lastStatusChange).toLocaleString()}</p><br><sub>${spot.provider} | N ${spot.latitude.toFixed(6)} | W ${spot.longitude.toFixed(6)}</sub>
+			<br><br><a target="_blank" href="https://www.google.com/maps/search/?api=1&query=${spot.latitude}%2C${spot.longitude}" style="border-radius: 6px; background-color: ${spot.occupied ? "#C42021" : Date.now() - spot.lastStatusChange > 15724800000 ? "#8c8c8c" : "#49c420"}; width: 260px !important; padding:5px;color:white;">Naviguj ma</a>`).addTo(Map);
 		});
+		if(!navigator.geolocation) document.querySelector("#getGeo")?.remove();
 	});
+	let started: boolean;
+	function getLocation() {
+  			try{
+				if(started) return;
+				const interval = setInterval(()=>{
+					navigator.geolocation.getCurrentPosition(data=>{renderLocation(data)}, ()=>{clearInterval(interval)});
+				},5000);
+			}catch(e){
+				console.log(e);
+			}
+		};
+		async function renderLocation(data: any){
+			started = true;
+			const { marker, layerGroup } = await import('leaflet');
+			if(group) group.remove();
+			group = layerGroup().addLayer(marker([data.coords.latitude, data.coords.longitude])).addTo(mapp);
+		}
 </script>
+
+<button id="getGeo" on:click={() => {getLocation()}} class="fixed left-40 top-4 fill-white z-20 w-12 h-12 bg-blue p-3 rounded-md">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Free 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --><path d="M256 0c17.7 0 32 14.3 32 32V66.7C368.4 80.1 431.9 143.6 445.3 224H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H445.3C431.9 368.4 368.4 431.9 288 445.3V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V445.3C143.6 431.9 80.1 368.4 66.7 288H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H66.7C80.1 143.6 143.6 80.1 224 66.7V32c0-17.7 14.3-32 32-32zM128 256c0 70.7 57.3 128 128 128s128-57.3 128-128s-57.3-128-128-128s-128 57.3-128 128zm128 80c-44.2 0-80-35.8-80-80s35.8-80 80-80s80 35.8 80 80s-35.8 80-80 80z"/></svg>
+</button>
 
 <div bind:this={mapElement} class="h-screen z-10" />
 
